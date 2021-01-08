edb33f1909c2cbbc0dcde3e470290de1251a41bf
daniel40392 <daniel40392@gmail.com>
2021-01-08 19:47:29 +0000

    reverted server settin

--- server.js
+++ server.js
@@ -52,20 +52,20 @@ app.get("/", function(req, res) {
 
 app.get('/db-get', (req, result) => {
   // for local...
-  const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-  });
+  // const pool = new Pool({
+  //   user: process.env.DB_USER,
+  //   host: process.env.DB_HOST,
+  //   database: process.env.DB_NAME,
+  //   password: process.env.DB_PASSWORD,
+  //   port: process.env.DB_PORT
+  // });
   // for prod
-//   const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+  const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
   pool.connect().then(client => {
     return client.query('SELECT * FROM matches')
       .then(res => {

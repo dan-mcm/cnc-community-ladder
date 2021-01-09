d89949405f8f4c3db3027703fe91920c31feacc6
daniel40392 <daniel40392@gmail.com>
2021-01-09 18:29:14 +0000

    accouting for broken endpoints

--- server.js
+++ server.js
@@ -44,5 +44,9 @@ app.get('/db-get', (req, result) => {
 
 app.get('/health', (req, res) => {res.sendStatus(200)})
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.listen(port);
 console.log('App is listening on port ' + port)

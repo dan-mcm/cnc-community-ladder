422442c5e65878105463792c00a7e9879eb7b486
daniel40392 <daniel40392@gmail.com>
2021-01-10 18:32:49 +0000

    updated social section with site links

--- src/containers/Social.js
+++ src/containers/Social.js
@@ -5,11 +5,27 @@ import { twitchData } from '../utils/socialData';
 import twitch from '../images/twitch.png';
 import discord from '../images/discord.png';
 import youtube from '../images/youtube.png';
+import community from '../images/community.png'
 
 class Social extends Component {
   render() {
     return (
       <Wrapper>
+      <div>
+        <StandardImage src={community} alt="Community" />
+        <Flex flexWrap="wrap">
+          <Box px={2} py={3} width={[1, 1 / 2]}>
+            <StyledLink href="https://www.redalertpros.com/">
+              Red Alert Pros</StyledLink><br/>For all your Red Alert Remastered Needs
+          </Box>
+          <Box px={2} py={3} width={[1, 1 / 2]}>
+            <StyledLink href="https://discord.gg/ScPuahJQPM">
+              C&C Community</StyledLink><br/>For all things C&C Related
+          </Box>
+        </Flex>
+      </div>
+      <br/>
+      <hr/>
         <div>
           <StandardImage src={discord} alt="Discord" />
           <Flex flexWrap="wrap">
@@ -61,7 +77,6 @@ class Social extends Component {
               </StyledLink>
             </Box>
             </Flex>
-
         </div>
         <br />
       </Wrapper>
